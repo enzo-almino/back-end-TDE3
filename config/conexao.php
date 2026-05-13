0b45ae2be34da29447f5e8e23552d27442d9fc26
F4bricioSantos <fd730041@gmail.com>
2026-05-13 14:58:04 -0300

    Conexão com supabase

---
+++ config/conexao.php
@@ -0,0 +1,20 @@
+<?php
+
+$host = 'aws-1-sa-east-1.pooler.supabase.com';
+$port = '6543';
+$db   = 'postgres';
+$user = 'postgres.ffcjaterxosxlqvcgmiw'; 
+$pass = 'EPpoaDY3MGUik3hZ';
+
+try {
+    $dsn = "pgsql:host=$host;port=$port;dbname=$db";
+    
+    $pdo = new PDO($dsn, $user, $pass, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, 
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, 
+        PDO::ATTR_EMULATE_PREPARES => false, 
+    ]);
+
+} catch (PDOException $e) {
+    die("Falha ao conectar ao Supabase: " . $e->getMessage());
+}
